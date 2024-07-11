dfe533b7135d86af25c9a49ac5964de51a27bc2b
unknown <hasidusandeepa24@gmail.com>
2024-07-11 23:48:17 +0530

    Initial commit

---
+++ VP_LAB6_q1/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace VP_LAB6_q1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnic = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbgender = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbyear = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbmonth = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lbdate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(218, 90);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(440, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Identify Data Retrieval System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 228);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Insert your NIC number";
+            // 
+            // txtnic
+            // 
+            this.txtnic.Location = new System.Drawing.Point(96, 276);
+            this.txtnic.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnic.Name = "txtnic";
+            this.txtnic.Size = new System.Drawing.Size(313, 28);
+            this.txtnic.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 400);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(521, 246);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Geneder:";
+            // 
+            // lbgender
+            // 
+            this.lbgender.AutoSize = true;
+            this.lbgender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbgender.Location = new System.Drawing.Point(656, 246);
+            this.lbgender.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbgender.Name = "lbgender";
+            this.lbgender.Size = new System.Drawing.Size(0, 22);
+            this.lbgender.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(521, 310);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 22);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Year:";
+            // 
+            // lbyear
+            // 
+            this.lbyear.AutoSize = true;
+            this.lbyear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbyear.Location = new System.Drawing.Point(656, 310);
+            this.lbyear.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbyear.Name = "lbyear";
+            this.lbyear.Size = new System.Drawing.Size(0, 22);
+            this.lbyear.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(521, 370);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 22);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Month:";
+            // 
+            // lbmonth
+            // 
+            this.lbmonth.AutoSize = true;
+            this.lbmonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbmonth.Location = new System.Drawing.Point(656, 370);
+            this.lbmonth.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbmonth.Name = "lbmonth";
+            this.lbmonth.Size = new System.Drawing.Size(0, 22);
+            this.lbmonth.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(521, 428);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 22);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Date:";
+            // 
+            // lbdate
+            // 
+            this.lbdate.AutoSize = true;
+            this.lbdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbdate.Location = new System.Drawing.Point(656, 428);
+            this.lbdate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbdate.Name = "lbdate";
+            this.lbdate.Size = new System.Drawing.Size(0, 22);
+            this.lbdate.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.ClientSize = new System.Drawing.Size(824, 502);
+            this.Controls.Add(this.lbdate);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lbmonth);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lbyear);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbgender);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtnic);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnic;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbgender;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbyear;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbmonth;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lbdate;
+    }
+}
+
---
+++ VP_LAB6_q1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VP_LAB6_q1
+{
+    public partial class Form1 : Form
+    {
+        int bodpart = 0;
+        bool isleapYear=false;
+        String gender ="";
+        int[] notleapyear ={ 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
+        string[] monthNames = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+        int[] leapyear = { 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           
+
+            String nic = txtnic.Text;
+            if (nic.Length == 9)
+            {
+                nicyear(nic);
+                nicgender(nic);
+                nicbod(nic);
+            }
+            else if (nic.Length == 12)
+            {
+                nicyear(nic);
+                nicgender(nic);
+                nicbod(nic);
+
+            }
+            else
+            {
+
+            }
+        }
+
+        private void nicyear(String nic)
+        {
+            if (nic.Length == 9)
+            {
+                int y = int.Parse(nic.Substring(0, 2));
+                lbyear.Text = (y + 1900).ToString();
+                checkyear(y + 1900);
+            }
+            else
+            {
+                int y = int.Parse(nic.Substring(0, 4));
+                checkyear(y);
+                lbyear.Text = y.ToString();
+            }
+
+        }
+
+        private void nicgender(string nic)
+        {
+            if (nic.Length == 9)
+            {
+                int y = int.Parse(nic.Substring(2, 3));
+                if (y > 500)
+                {
+                    bodpart = y - 500;
+                    lbgender.Text = "Female";
+                    gender = "Female";
+                }
+                else
+                {
+                    bodpart = y;
+                    lbgender.Text = "Male";
+                    gender = "Male";
+                }
+            }
+            else
+            {
+                int y = int.Parse(nic.Substring(4, 3));
+                if (y > 500)
+                {
+                    bodpart = y - 500;
+                    lbgender.Text = "Female";
+                    gender = "Female";
+                }
+                else
+                {
+                    bodpart = y;
+                    lbgender.Text = "Male";
+                    gender = "Male";
+                }
+            }
+        }
+        private void checkyear(int year)
+        {
+            
+            if (year % 400 == 0 & year % 4 == 0)
+            {
+                isleapYear = true;
+            }
+            else
+            {
+                isleapYear= false;
+            }
+
+        }
+        private void nicbod(String nic)
+        {
+            int days = bodpart-1;
+
+            if (isleapYear)
+            {
+                for (int i = 0; i < leapyear.Length; i++)
+                {
+                    if (days <= leapyear[i])
+                    {
+                        lbmonth.Text = monthNames[i];
+                        lbdate.Text = (i == 0 ? days : days - leapyear[i - 1]).ToString();
+
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < notleapyear.Length; i++)
+                {
+                    if (days <= notleapyear[i])
+                    {
+                        lbmonth.Text = monthNames[i];
+                        lbdate.Text = (i == 0 ? days : days - notleapyear[i - 1]).ToString();
+                       
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+}
